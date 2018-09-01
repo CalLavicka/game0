305fbd9ce3e0378143cab95c89c9ed9f8c2bf1d5
Cal Lavicka <cal.lavicka@gmail.com>
2018-09-01 19:25:34 -0400

    Fixed export-meshes.py

--- meshes/export-meshes.py
+++ meshes/export-meshes.py
@@ -100,8 +100,8 @@ for name in to_write:
 			#TODO: set 'col' based on object's active vertex colors array.
 			# you should be able to use code much like the texcoord code below.
 			#col = mathutils.Color((1.0, 1.0, 1.0))
-			if mesh.vertex_color:
-				col = mathutils.Color((mesh.vertex_color.active.data[loop.vertex_index].color))
+			if mesh.vertex_colors:
+				col = mathutils.Color((mesh.vertex_colors.active.data[loop.vertex_index].color))
 			else:
 				col = mathutils.Color((1.0, 1.0, 1.0))
 			data += struct.pack('BBBB', int(col.r * 255), int(col.g * 255), int(col.b * 255), 255)
